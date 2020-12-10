3ddec401d4de21fb5c48f2849d552a1e2c57d45d
wh-lv <palomawhl@163.com>
2020-12-11 01:28:27 +0800

    '描述'

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,18 @@
+/**
+ * vue-router源码实现
+ * 1. 生成 path 为 key，component 为 value的键值对对象
+ * 2. 事项 router-link 和 router-view 组件
+ * 3. 监听 url 的变化，将当前的 path 值赋予 current，current为响应式
+ * 4. 
+ */
+
 let Vue;
 
 class VueRouter {
     constructor (options) {
         this.$options = options;
     }
+    
 }
 
 // 将 VueRouter 作为一个组件
